4015a13922aaf3d68dd80f2c0316916e0b231da9
BayKanguru <bekirberke11111@gmail.com>
2021-05-08 19:16:09 +0300

    Tidied up the general files

--- js/eba.js
+++ js/eba.js
@@ -1,7 +1,7 @@
 var style = document.createElement("style");
 style.id = "added_by_eba-dark-mode";
 
-fetch(chrome.extension.getURL("eba.css"))
+fetch(chrome.extension.getURL("../eba.css"))
   .then((response) => response.text())
   .then((data) => {
     style.innerHTML = data.toString();
